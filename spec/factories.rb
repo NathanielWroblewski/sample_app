083d8d6ae8e1313c63cdae4def8092d6c2bd84bd
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2012-11-22 11:27:32 -0600

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name "Nathaniel Wroblewski"
+		email "NathanielWroblewski@gmail.com"
+		password "sotnem55"
+		password_confirmation "sotnem55"
+	end
+end
